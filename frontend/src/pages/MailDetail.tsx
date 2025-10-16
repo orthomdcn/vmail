b60606a848b985d1e41ae5289a2a1486dca42cd8
bonecn <gujiangjiang@sohu.com>
2025-10-17 03:34:16 +0800

    Revert "fix: 修复了邮件列表和邮件详情的INBOX栏高度不一致的问题"
    This reverts commit 0c3388be9554b191f6051195a93682b52fdcdd0a.

--- frontend/src/pages/MailDetail.tsx
+++ frontend/src/pages/MailDetail.tsx
@@ -42,8 +42,7 @@ export function MailDetail({ email, onClose }: MailDetailProps) {
       <div className="flex-1 flex text-sm bg-[#ffffffd6] backdrop-blur-xl rounded-md min-h-0">
         <iframe
             srcDoc={email.html || `<pre>${email.text}</pre>`}
-            // fix: 将h-[60vh]修改为h-full，使其填充满父容器
-            className="w-full h-full border-0"
+            className="w-full h-[60vh] border-0"
             sandbox="allow-popups allow-popups-to-escape-sandbox"
             title="Email Content"
           />
